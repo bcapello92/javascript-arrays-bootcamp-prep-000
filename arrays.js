bb57818f03adee708c2001c31869c7fd1c533dc7
Brendan Capello <capello4@gmail.com>
2018-01-07 18:15:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+function destructivelyRemoveElmentFromEndOfArray(array){
+  array.pop()
+  return array
+}
